463fa0b173de0780d668adfd9123c34fb9dbc02f
Kelsey Cossifos <kcossifos@gmail.com>
2016-08-24 23:39:13 -0400

    increments version number

--- src/util.js
+++ src/util.js
@@ -1,8 +1,5 @@
 const colors = require('colors');
 
-// setup for file system
-const fs = require('fs');
-
 // set time stamp for logs
 const ts = new Date();
 
@@ -14,11 +11,67 @@ exports.debug = (data) => {
 
   // id debug mode equal true
   if (process.env.DEBUG) {
-    fs.appendFile('logs/err.log', out1, (err) => {
-      if (err) throw err;
-    // let dev know file was sucessfully appended
-      console.log(colors.cyan('log updated sucessfully'));
-    }); // closing append file
-  } // closing if debug process
-  console.log(out1);
+    console.log(colors.cyan('log updated sucessfully'));
+  } else {
+    console.warn(colors.yellow(out1));
+  }
 }; // closing debug export
+
+
+exports.versionNum = (parm1, parm2) => {
+  // makes sure parm2 is lower case
+  const parmCase = parm2.toLowerCase();
+  // spilts parm one to seperate each number
+  let splitNum = parm1.split('.');
+
+  // if parm1 is greater than 3 or less than one return an error
+  if (splitNum.length > 3 || splitNum.length < 1) {
+    return 'error';
+  }
+
+  // checks number
+  function checkIfNumber(num) {
+    if (Number(num) === 0) {
+      return true;
+    }
+    if (!Number(num)) {
+      return false;
+    }
+    return true;
+  }
+
+  for (let i = 0; i < splitNum.length; i++) {
+    if (!checkIfNumber(splitNum[i])) {
+      return 'error';
+    }
+  }
+
+  splitNum = splitNum.map(Number);
+  // increments major number
+  if (parmCase === 'major') {
+    splitNum[0]++;
+    for (let i = 1; i < splitNum.length; i++) {
+      splitNum[i] = 0;
+    }
+      // increments minor number
+  } else if (parmCase === 'minor') {
+    if (splitNum.length === 1) {
+      splitNum.push(0);
+    }
+    splitNum[1]++;
+    for (let i = 2; i < splitNum.length; i++) {
+      splitNum[i] = 0;
+    }
+      // increments patch number
+  } else if (parmCase === 'patch') {
+    if (splitNum.length === 2) {
+      splitNum.push(0);
+    }
+    splitNum[2]++;
+  } else {
+    return 'error';
+  }
+
+  // joins verison number
+  return splitNum.join('.');
+};
--- test/__util.js
+++ test/__util.js
@@ -1,6 +1,55 @@
 const util = require('../src/util');
+const sinon = require('sinon');
+const expect = require('chai').expect;
+
 
 describe('debug test', () => {
 //
   it('testing to see if stub displays', () => {});
+
+  it('Console logs', () => {
+    const log = sinon.spy();
+    log(util.debug('Logging'));
+    expect(log.callCount).to.equal(1);
+  });
+
+  it('Console warns', () => {
+    const warn = sinon.spy();
+    warn(util.debug('warn'));
+    expect(warn.callCount).to.equal(1);
+  });
+});
+
+// Tests verison bump
+describe('Verison Bump', () => {
+  const num = '1.9.0';
+  let versNum;
+
+  // Test if it increments major version bump
+  it('| Will increment the major verison number', (done) => {
+    versNum = util.versionNum(num, 'major');
+    expect(versNum).to.be.equal('2.0.0');
+    done();
+  });
+
+  // Test if it increments minor version bump
+  it('| Will increment the minor verison number', (done) => {
+    versNum = util.versionNum(num, 'minor');
+    expect(versNum).to.be.equal('1.10.0');
+    done();
+  });
+
+  // Test if it increments patch version bump
+  it('| Will increment the patch verison numbe', (done) => {
+    versNum = util.versionNum(num, 'patch');
+    expect(versNum).to.be.equal('1.9.1');
+    done();
+  });
+
+  // Test to make sure verison number is in the correct format
+  it('| Returns the error because version number is in wrong format', (done) => {
+    versNum = util.versionNum('1.w.3', 'patch');
+    expect(versNum).to.be.equal('error');
+    done();
+  });
 });
